5e87a81349d4677b3c855f538a9c98f76605c941
oyewale tayo <tayooyewale@gmail.com>
2020-05-11 16:26:33 +0100

    Updates

---
+++ precache-manifest.4e23475a5d4ce40942703b921a70404c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23629fb09fd72f9433069ede5bb52ad5",
+    "url": "/github.io/Robofriends/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/github.io/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "296ad65e148f13467f48",
+    "url": "/github.io/Robofriends/static/css/main.b264861d.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/github.io/Robofriends/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "296ad65e148f13467f48",
+    "url": "/github.io/Robofriends/static/js/main.80db366c.chunk.js"
+  },
+  {
+    "revision": "11c97c37ba56e1b3ae9d",
+    "url": "/github.io/Robofriends/static/js/runtime~main.1eb50dff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/github.io/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
